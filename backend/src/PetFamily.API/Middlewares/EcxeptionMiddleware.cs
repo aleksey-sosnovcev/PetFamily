1e48415b48fbd62ad4d1f8097c8f2d21fe4b6f89
aleksey-sosnovcev <aleksey_sosnovcev@mail.ru>
2025-07-31 18:37:59 +0300

    Serilog + ExceptionMiddleware

---
+++ backend/src/PetFamily.API/Middlewares/EcxeptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using PetFamily.API.Response;
+
+namespace PetFamily.API.Middlewares
+{
+    public class EcxeptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<EcxeptionMiddleware> _logger;
+
+        public EcxeptionMiddleware(RequestDelegate next, ILogger<EcxeptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,ex.Message);
+                var responseError = new ResponseError("server.internal", ex.Message, null);
+                var envelope = Envelope.Error([responseError]);
+            
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                await context.Response.WriteAsJsonAsync(ex);
+            }
+
+        }
+    }
+
+    public static class EcxeptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<EcxeptionMiddleware>();
+        }
+    }
+}
